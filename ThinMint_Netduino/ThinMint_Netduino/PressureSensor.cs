20f856f3c9a389d40cfbefddff0eb3cf077965e0
robocafaz <robocafaz@gmail.com>
2012-04-13 14:33:52 +0000

    New Sensors and Ports

--- ThinMint_Netduino/ThinMint_Netduino/PressureSensor.cs
+++ ThinMint_Netduino/ThinMint_Netduino/PressureSensor.cs
@@ -3,13 +3,14 @@ using Microsoft.SPOT;
 using Microsoft.SPOT.Hardware;
 using SecretLabs.NETMF.Hardware.Netduino;
 using System.Threading;
+using SecretLabs.NETMF.Hardware;
 
 namespace ThinMint_Netduino
 {
     class PressureSensor
     {
         private static OutputPort _input;
-        private static OutputPort _power;
+        private static OutputPort _output;
 
         /// <summary>
         /// Pin reading input from the PSI sensor.
@@ -23,16 +24,16 @@ namespace ThinMint_Netduino
         /// <summary>
         /// Pin providing power to the PSI sensor.
         /// </summary>
-        public static OutputPort Power
+        public static OutputPort Output
         {
-            get { return PressureSensor._power; }
-            set { PressureSensor._power = value; }
+            get { return PressureSensor._output; }
+            set { PressureSensor._output = value; }
         }
 
         public static void Launch()
         {
-            _input = new OutputPort(Pins.GPIO_PIN_A0, false);
-            _power = new OutputPort(Pins.GPIO_PIN_A1, false);
+            _input = new OutputPort(Pins.GPIO_PIN_D0, false);
+            _output = new OutputPort(Pins.GPIO_PIN_D1, false);
             (new Thread(new ThreadStart(Listen))).Start();
         }
 
@@ -40,8 +41,16 @@ namespace ThinMint_Netduino
         {
             while (true)
             {
-                Debug.Print("PSI: " + _input.Read());
+                Debug.Print("Pressure: " + _input.Read());
             }
+        }
+
+
+        private static float CalculateVoltage(float inputVoltage)
+        {
+            float outputVoltage;
+            outputVoltage = 0; // EQUATION GOES HERE
+            return outputVoltage;
         }
     }
 }
