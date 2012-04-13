2846ef4c4aa4afd102196caea23e191408a527bc
robocafaz <robocafaz@gmail.com>
2012-04-13 13:38:39 +0000

    Initial commit.

---
+++ ThinMint_Netduino/ThinMint_Netduino/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+
+namespace ThinMint_Netduino
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            OutputPort led = new OutputPort(Pins.ONBOARD_LED, false);
+            while (true)
+            {
+                Thread.Sleep(1000);
+                led.Write(!led.Read());
+            }
+        }
+
+    }
+}
