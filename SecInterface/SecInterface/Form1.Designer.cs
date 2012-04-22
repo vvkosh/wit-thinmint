6ebd5696073d52465131425473509488e72f4056
Jdtgreat <Jdtgreat@gmail.com>
2012-04-22 19:39:42 +0000

    added Interface

---
+++ SecInterface/SecInterface/Form1.Designer.cs
@@ -0,0 +1,369 @@
+﻿namespace SecInterface
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.CurrentPSIOut = new System.Windows.Forms.Label();
+            this.CurrentRPMOut = new System.Windows.Forms.Label();
+            this.ProsRPMOut = new System.Windows.Forms.Label();
+            this.ProsPSIOut = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Chart = new AxMSChart20Lib.AxMSChart();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Chart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(123, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(185, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Current";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(568, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(275, 55);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Prospective";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(147, 160);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "PSI";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(250, 160);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "RPM";
+            // 
+            // CurrentPSIOut
+            // 
+            this.CurrentPSIOut.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.CurrentPSIOut.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.CurrentPSIOut.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CurrentPSIOut.Location = new System.Drawing.Point(127, 111);
+            this.CurrentPSIOut.Name = "CurrentPSIOut";
+            this.CurrentPSIOut.Size = new System.Drawing.Size(88, 33);
+            this.CurrentPSIOut.TabIndex = 8;
+            // 
+            // CurrentRPMOut
+            // 
+            this.CurrentRPMOut.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.CurrentRPMOut.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.CurrentRPMOut.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CurrentRPMOut.Location = new System.Drawing.Point(226, 111);
+            this.CurrentRPMOut.Name = "CurrentRPMOut";
+            this.CurrentRPMOut.Size = new System.Drawing.Size(88, 33);
+            this.CurrentRPMOut.TabIndex = 9;
+            // 
+            // ProsRPMOut
+            // 
+            this.ProsRPMOut.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ProsRPMOut.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.ProsRPMOut.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ProsRPMOut.Location = new System.Drawing.Point(702, 111);
+            this.ProsRPMOut.Name = "ProsRPMOut";
+            this.ProsRPMOut.Size = new System.Drawing.Size(88, 33);
+            this.ProsRPMOut.TabIndex = 11;
+            // 
+            // ProsPSIOut
+            // 
+            this.ProsPSIOut.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ProsPSIOut.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.ProsPSIOut.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ProsPSIOut.Location = new System.Drawing.Point(603, 111);
+            this.ProsPSIOut.Name = "ProsPSIOut";
+            this.ProsPSIOut.Size = new System.Drawing.Size(88, 33);
+            this.ProsPSIOut.TabIndex = 10;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(737, 312);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 12;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(733, 160);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 20);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "RPM";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(630, 160);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 20);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "PSI";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(737, 402);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(42, 20);
+            this.textBox2.TabIndex = 16;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Chart
+            // 
+            this.Chart.DataSource = null;
+            this.Chart.Location = new System.Drawing.Point(35, 183);
+            this.Chart.Name = "Chart";
+            this.Chart.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("Chart.OcxState")));
+            this.Chart.Size = new System.Drawing.Size(656, 401);
+            this.Chart.TabIndex = 15;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(785, 402);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(42, 20);
+            this.textBox3.TabIndex = 17;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(737, 428);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(42, 20);
+            this.textBox4.TabIndex = 19;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(785, 428);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(42, 20);
+            this.textBox5.TabIndex = 18;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(737, 454);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(42, 20);
+            this.textBox6.TabIndex = 21;
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(785, 454);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(42, 20);
+            this.textBox7.TabIndex = 20;
+            this.textBox7.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(737, 480);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(42, 20);
+            this.textBox8.TabIndex = 23;
+            this.textBox8.TextChanged += new System.EventHandler(this.textBox8_TextChanged);
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(785, 480);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(42, 20);
+            this.textBox9.TabIndex = 22;
+            this.textBox9.TextChanged += new System.EventHandler(this.textBox9_TextChanged);
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(737, 506);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(42, 20);
+            this.textBox10.TabIndex = 25;
+            this.textBox10.TextChanged += new System.EventHandler(this.textBox10_TextChanged);
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(785, 506);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(42, 20);
+            this.textBox11.TabIndex = 24;
+            this.textBox11.TextChanged += new System.EventHandler(this.textBox11_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(122, 598);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(24, 13);
+            this.label6.TabIndex = 26;
+            this.label6.Text = "PSI";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Green;
+            this.pictureBox1.Location = new System.Drawing.Point(80, 591);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(36, 26);
+            this.pictureBox1.TabIndex = 27;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.pictureBox2.Location = new System.Drawing.Point(258, 591);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(36, 26);
+            this.pictureBox2.TabIndex = 28;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(300, 598);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(31, 13);
+            this.label7.TabIndex = 29;
+            this.label7.Text = "RPM";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(949, 635);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.Chart);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ProsRPMOut);
+            this.Controls.Add(this.ProsPSIOut);
+            this.Controls.Add(this.CurrentRPMOut);
+            this.Controls.Add(this.CurrentPSIOut);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Wobbler UI";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Chart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label CurrentPSIOut;
+        private System.Windows.Forms.Label CurrentRPMOut;
+        private System.Windows.Forms.Label ProsRPMOut;
+        private System.Windows.Forms.Label ProsPSIOut;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox2;
+        private AxMSChart20Lib.AxMSChart Chart;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ SecInterface/SecInterface/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SecInterface
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            CurrentPSIOut.Text = textBox1.Text;
+            CurrentRPMOut.Text = textBox1.Text;
+            ProsPSIOut.Text = textBox1.Text;
+            ProsRPMOut.Text = textBox1.Text;
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            Chart.Row = 1;
+            Chart.Column = 1;
+            Chart.Data = textBox2.Text;
+        }
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            Chart.Row = 1;
+            Chart.Column = 2;
+            Chart.Data = textBox3.Text;
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            Chart.Row = 2;
+            Chart.Column = 1;
+            Chart.Data = textBox4.Text;
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            Chart.Row = 2;
+            Chart.Column = 2;
+            Chart.Data = textBox5.Text;
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+            Chart.Row = 3;
+            Chart.Column = 1;
+            Chart.Data = textBox6.Text;
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+            Chart.Row = 3;
+            Chart.Column = 2;
+            Chart.Data = textBox7.Text;
+        }
+
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        {
+            Chart.Row = 4;
+            Chart.Column = 1;
+            Chart.Data = textBox8.Text;
+        }
+
+        private void textBox9_TextChanged(object sender, EventArgs e)
+        {
+            Chart.Row = 4;
+            Chart.Column = 2;
+            Chart.Data = textBox9.Text;
+        }
+
+        private void textBox10_TextChanged(object sender, EventArgs e)
+        {
+            Chart.Row = 5;
+            Chart.Column = 1;
+            Chart.Data = textBox10.Text;
+        }
+
+        private void textBox11_TextChanged(object sender, EventArgs e)
+        {
+            Chart.Row = 5;
+            Chart.Column = 2;
+            Chart.Data = textBox11.Text;
+        }
+    }
+}
